00a70bf77931cc89c43922b6e5459880aa1cf35a
nanigasi_san <nanigasi.py@gmail.com>
2025-11-05 01:58:41 +0900

    fix: テスト動作時に開発者モードをオンにするようにした

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -75,11 +75,18 @@ void main() {
 
     // Build our app and trigger a frame.
     await tester.pumpWidget(ArgusApp(controller: controller));
+    
+    // Wait for the widget to build
+    await tester.pumpAndSettle();
 
     // Verify that the app title 'Argus' is displayed
     expect(find.text('Argus'), findsWidgets);
 
-    // Verify that the status information is displayed
+    // Enable developer mode to show detailed status
+    controller.setDeveloperMode(true);
+    await tester.pumpAndSettle();
+
+    // Verify that the status information is displayed (only visible in developer mode)
     expect(find.textContaining('Current state'), findsOneWidget);
   });
 }
