3a9a5feebd609b9a0c5816c8de03e0c2442342ae
nanigasi_san <nanigasi.py@gmail.com>
2026-04-04 22:37:21 +0900

    Update Android release config and GeoJSON QR demo inputs (#38)
    * 一旦aabビルド出来るようになった
    
    * add privacy
    
    * feat: GeoJSON QRデモで引数入力を受け付ける

--- android/app/src/main/kotlin/com/argus/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.argus
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
---
+++ android/app/src/main/kotlin/com/argus/orienteering/MainActivity.kt
@@ -0,0 +1,8 @@
+package com.argus.orienteering
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
+
+
+
